b93da4445b71c1b756e45bc5250268b0d9b75164
Abigayle Peterson <abigaylerose03@gmail.com>
2020-03-16 21:02:36 -0700

    setup initial game window

---
+++ src/main/java/Program.java
@@ -0,0 +1,10 @@
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+    // ctrl + option + o to remove unused import statements
+    // red indicates that the import statement needs to be used
+    public static void main(String[] args) {
+        Game.init(args);
+        Game.start();
+    }
+}
